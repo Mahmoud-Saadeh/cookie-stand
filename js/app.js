041c92fc208bb093405a719579dcbeee712263fd
Mahmoud Saadeh <mahmoud.saadeh998@gmail.com>
2021-04-04 15:07:44 +0300

    Added Dubai

--- js/app.js
+++ js/app.js
@@ -141,3 +141,75 @@ for (let index = 0; index < tokyo.openTimes.length; index++) {
 let tokyoLiTotal = document.createElement('li');
 tokyoUl.appendChild(tokyoLiTotal);
 tokyoLiTotal.textContent = `Total: ${tokyo.totalCookie()} cookies`;
+
+///////////////////// Dubai
+let dubai = {
+  name: 'Dubai',
+  openTimes: [
+    '6am',
+    '7am',
+    '8am',
+    '9am',
+    '10am',
+    '11am',
+    '12pm',
+    '1pm',
+    '2pm',
+    '3pm',
+    '4pm',
+    '5pm',
+    '6pm',
+    '7pm',
+  ],
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7,
+  randomCustNum: function () {
+    return (
+      Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust
+    );
+  },
+  hourlyCust: function () {
+    let hourlyCust = [];
+    for (let i = 0; i < this.openTimes.length; i++) {
+      hourlyCust.push(this.randomCustNum());
+    }
+    return hourlyCust;
+  },
+  hourlyAvgCookie: function () {
+    let hourlyCust = this.hourlyCust();
+    let hourlyAvgCookieArr = [];
+    for (let j = 0; j < hourlyCust.length; j++) {
+      hourlyAvgCookieArr.push(Math.floor(hourlyCust[j] * this.avgCookie));
+    }
+    return hourlyAvgCookieArr;
+  },
+  totalCookie: function () {
+    let hourlyAvgCookie = this.hourlyAvgCookie();
+    let total = 0;
+    for (let i = 0; i < hourlyAvgCookie.length; i++) {
+      total = total + hourlyAvgCookie[i];
+    }
+    return total;
+  },
+};
+
+let dubaiParentElement = document.getElementById('parent');
+let dubaihead = document.createElement('h2');
+dubaiParentElement.appendChild(dubaihead);
+
+dubaihead.textContent = dubai.name;
+
+let dubaiUl = document.createElement('ul');
+dubaiParentElement.appendChild(dubaiUl);
+
+for (let index = 0; index < dubai.openTimes.length; index++) {
+  let dubaiLi = document.createElement('li');
+  dubaiUl.appendChild(dubaiLi);
+  dubaiLi.textContent = `${dubai.openTimes[index]}: ${
+    dubai.hourlyAvgCookie()[index]
+  } cookies`;
+}
+let dubaiLiTotal = document.createElement('li');
+dubaiUl.appendChild(dubaiLiTotal);
+dubaiLiTotal.textContent = `Total: ${dubai.totalCookie()} cookies`;
