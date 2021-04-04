0502fc2a4817c3dbb07843a16507e4b390356a1a
Mahmoud Saadeh <mahmoud.saadeh998@gmail.com>
2021-04-04 15:01:23 +0300

    Added Tokyo

--- js/app.js
+++ js/app.js
@@ -69,3 +69,75 @@ for (let index = 0; index < seattle.openTimes.length; index++) {
 let seattleLiTotal = document.createElement('li');
 seattleUl.appendChild(seattleLiTotal);
 seattleLiTotal.textContent = `Total: ${seattle.totalCookie()} cookies`;
+
+///////////////////// Tokyo
+let tokyo = {
+  name: 'Tokyo',
+  openTimes: [
+    '6am',
+    '7am',
+    '8am',
+    '9am',
+    '10am',
+    '11am',
+    '12pm',
+    '1pm',
+    '2pm',
+    '3pm',
+    '4pm',
+    '5pm',
+    '6pm',
+    '7pm',
+  ],
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2,
+  randomCustNum: function () {
+    return (
+      Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust
+    );
+  },
+  hourlyCust: function () {
+    let hourlyCust = [];
+    for (let i = 0; i < this.openTimes.length; i++) {
+      hourlyCust.push(this.randomCustNum());
+    }
+    return hourlyCust;
+  },
+  hourlyAvgCookie: function () {
+    let hourlyCust = this.hourlyCust();
+    let hourlyAvgCookieArr = [];
+    for (let j = 0; j < hourlyCust.length; j++) {
+      hourlyAvgCookieArr.push(Math.floor(hourlyCust[j] * this.avgCookie));
+    }
+    return hourlyAvgCookieArr;
+  },
+  totalCookie: function () {
+    let hourlyAvgCookie = this.hourlyAvgCookie();
+    let total = 0;
+    for (let i = 0; i < hourlyAvgCookie.length; i++) {
+      total = total + hourlyAvgCookie[i];
+    }
+    return total;
+  },
+};
+
+let tokyoParentElement = document.getElementById('parent');
+let tokyohead = document.createElement('h2');
+tokyoParentElement.appendChild(tokyohead);
+
+tokyohead.textContent = tokyo.name;
+
+let tokyoUl = document.createElement('ul');
+tokyoParentElement.appendChild(tokyoUl);
+
+for (let index = 0; index < tokyo.openTimes.length; index++) {
+  let tokyoLi = document.createElement('li');
+  tokyoUl.appendChild(tokyoLi);
+  tokyoLi.textContent = `${tokyo.openTimes[index]}: ${
+    tokyo.hourlyAvgCookie()[index]
+  } cookies`;
+}
+let tokyoLiTotal = document.createElement('li');
+tokyoUl.appendChild(tokyoLiTotal);
+tokyoLiTotal.textContent = `Total: ${tokyo.totalCookie()} cookies`;
